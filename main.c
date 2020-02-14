2e5cfbb06ca912173b15b3ea458c70d66aa97a04
md-dabovi <52411658+md-dabovi@users.noreply.github.com>
2020-02-14 16:47:56 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,43 @@
 #include <libc.h>
+#include <stdio.h>
 #include "get_next_line.h"
 
 int main(int ac, char **av)
 {
-	(void)ac;
-    char *line;
-    int fd1 = open(av[1], O_RDONLY);
-    int ret1;
+	char *line;
+	int *fd;
+	int *ret;
+	int nb; //nombre file descriptor
+	int v;
+	int i;
+
+	if (ac < 2)
+		return (-1);
+	if (!(fd = malloc(sizeof(int) * ac - 1)))
+		return (-1);
+	if (!(ret = malloc(sizeof(int) * ac - 1)))
+		return (-1);
+	nb = 0;
+	i = 0;
+	while (nb < ac)
+	{
+		fd[nb] = open(av[nb + 1], O_RDONLY);
+		nb++;
+	}
 	do
 	{
-		ret1 = get_next_line(fd1, &line);
-		printf("1 | %s\n", line);
-	} while (ret1);
+		i++;
+		printf("Ligne %d --------------\n", i);
+		nb = 0;
+		v = 0;
+		while (nb < ac - 1)
+		{
+			ret[nb] = get_next_line(fd[nb], &line);
+			printf(" FD %d   | %s\n", nb + 1, line);
+			if (ret[nb] > 0)
+				v++;
+			nb++;
+		}
+	} while (v > 0);
+	return (0);
 }
